2e66843ce1001f2b429aa712cf6221a425ab1365
Nguyễn Văn Thọ <56969447+VANTHO15@users.noreply.github.com>
2023-04-14 12:48:43 +0700

    Add files via upload

---
+++ Plugins_HuLa/Port_HuLa/generate_PC/include/Test.h
@@ -0,0 +1,18 @@
+[!CODE!][!//
+[!AUTOSPACING!]
+
+
+#ifndef AAAA
+#define AAAA
+
+
+
+[!CODE!]
+#define SYSTEM_Port_ENABLE                 ([!IF "PortGeneral/TestEnable"!]STD_ON[!ELSE!]STD_OFF[!ENDIF!])
+[!ENDCODE!]
+ 
+
+
+#endif 
+
+[!ENDCODE!]
---
+++ Plugins_HuLa/Port_HuLa/generate_PC/src/Test.c
@@ -0,0 +1,7 @@
+
+#include "Test.h"
+
+[!CODE!]
+#define SYSTEM_Port_ENABLE                 ([!IF "PortGeneral/TestEnable"!]STD_ON[!ELSE!]STD_OFF[!ENDIF!])
+[!ENDCODE!]
+
